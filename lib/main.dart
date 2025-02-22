d7c256351effdcbb8fda046f8c749d26cfe84edd
juanfra312003 <juanframireze@javeriana.edu.co>
2025-02-22 11:34:11 -0500

    Update .env management

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,30 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:spl_front/bloc/ui_management/chat/chat_bloc.dart';
-import 'package:spl_front/bloc/ui_management/labels_store/labels_store_bloc.dart';
-import 'package:spl_front/bloc/ui_management/order_tracking/order_tracking_bloc.dart';
 import 'package:spl_front/bloc/ui_management/chats/chats_bloc.dart';
+import 'package:spl_front/bloc/ui_management/gps/gps_bloc.dart';
+import 'package:spl_front/bloc/ui_management/labels_store/labels_store_bloc.dart';
+import 'package:spl_front/bloc/ui_management/location/location_bloc.dart';
+import 'package:spl_front/bloc/ui_management/map/map_bloc.dart';
+import 'package:spl_front/bloc/ui_management/order_tracking/order_tracking_bloc.dart';
 import 'package:spl_front/bloc/ui_management/orders_list/orders_list_bloc.dart';
 import 'package:spl_front/bloc/ui_management/profile_tab/profile_tab_bloc.dart';
+import 'package:spl_front/bloc/ui_management/search_places/search_places_bloc.dart';
 import 'package:spl_front/pages/login/login_page.dart';
 import 'package:spl_front/pages/login/login_page_variant.dart';
 import 'package:spl_front/pages/login_page_web.dart';
 import 'package:spl_front/providers/product_form_provider.dart';
 import 'package:spl_front/providers/selected_labels_provider.dart';
 import 'package:spl_front/routes/routes.dart';
+import 'package:spl_front/services/gui/map/map_service.dart';
 import 'package:spl_front/spl/spl_variables.dart';
 
-void main() {
+Future main() async {
+  // Load the environment variables from the .env file for begin the app
+  await dotenv.load(fileName: '.env');
   runApp(MyApp());
 }
 
@@ -29,6 +37,15 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       // Providers using BLoC, managed on the folder lib/bloc/...
       providers: [
+        // Providers for manage the Location, GPS and Map on the App
+        BlocProvider(create: (_) => GpsBloc()),
+        BlocProvider(create: (_) => LocationBloc()),
+        BlocProvider(
+            create: (_) =>
+                MapBloc(locationBloc: BlocProvider.of<LocationBloc>(context))),
+        BlocProvider(create: (_) => SearchPlacesBloc(mapService: MapService())),
+
+        // Providers for UI Management
         BlocProvider(create: (_) => ProfileTabBloc()),
         BlocProvider(create: (_) => LabelsStoreBloc()),
         ChangeNotifierProvider(create: (_) => ProductFormProvider()),
@@ -36,7 +53,7 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (_) => ChatBloc()),
         BlocProvider(create: (_) => OrderStatusBloc()),
         BlocProvider(create: (_) => ChatsBloc()),
-        BlocProvider(create: (_) => OrderListBloc())
+        BlocProvider(create: (_) => OrderListBloc()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
